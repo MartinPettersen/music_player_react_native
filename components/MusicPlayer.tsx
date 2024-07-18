456100da766023f7733be199beeaf1d73f0472f7
MartinPettersen <searchingtheskyfor@gmail.com>
2024-07-18 08:53:44 +0200

    add if playing a song and choosing a new song then start playing the new sound

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -4,42 +4,52 @@ import { Audio } from "expo-av";
 import { Feather } from "@expo/vector-icons";
 import Bar from "./Bar";
 import MenuButton from "./MenuButton";
-import * as DocumentPicker from 'expo-document-picker';
-import * as FileSystem from 'expo-file-system';
-
+import * as DocumentPicker from "expo-document-picker";
+import * as FileSystem from "expo-file-system";
 
 const MusicPlayer = () => {
   const [soundt, setSoundT] = useState<Audio.Sound | null>(null);
   const [duration, setDuration] = useState<number | undefined>(0);
   const [playing, setPlaying] = useState<boolean>(false);
   const [position, setPosition] = useState<any | null>(0);
-  const [musicFile, setMusicFile] = useState<string>("../assets/alex-productions-cinematic-epic-emotional-eglair.mp3");
-  const [songName, setSongName] = useState<string>("")
+  const [musicFile, setMusicFile] = useState<string>(
+    "../assets/alex-productions-cinematic-epic-emotional-eglair.mp3"
+  );
+  const [songName, setSongName] = useState<string>("");
+  const [newSong, setNewSong] = useState<boolean>(false);
 
   const chooseFile = async () => {
     try {
-      const result = await DocumentPicker.getDocumentAsync({})
-      if(result.assets !== null) {
+      const result = await DocumentPicker.getDocumentAsync({});
+      if (result.assets !== null) {
         const { uri } = result.assets[0];
-        setSongName(result.assets[0].name)
-        setMusicFile(uri)
+        setSongName(result.assets[0].name);
+        setMusicFile(uri);
+        const { sound } = await Audio.Sound.createAsync({ uri: uri });
+        setSoundT(sound);
+        const status = await sound.getStatusAsync();
+        if (status.isLoaded) {
+          setDuration(status.playableDurationMillis);
+        }
+        setNewSong(false);
+        await sound.playAsync();
       }
-      
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
+
+  };
+
 
   async function playSound() {
-    if (soundt === null) {
-      const { sound } = await Audio.Sound.createAsync(
-        { uri: musicFile }
-      );
+    if (soundt === null || newSong == true) {
+      const { sound } = await Audio.Sound.createAsync({ uri: musicFile });
       setSoundT(sound);
       const status = await sound.getStatusAsync();
       if (status.isLoaded) {
         setDuration(status.playableDurationMillis);
       }
+      setNewSong(false);
       await sound.playAsync();
     } else {
       if (!playing) {
@@ -61,33 +71,31 @@ const MusicPlayer = () => {
     setPlaying(!playing);
   }
 
-  const updateTimer = async() => {
+  const updateTimer = async () => {
     const status = await soundt!.getStatusAsync();
     if (status.isLoaded) {
       setPosition(status.positionMillis);
     }
-
-  }
+  };
 
   useEffect(() => {
     let timer: any;
-    
+
     if (playing) {
-      timer = setInterval(updateTimer, 66)
+      timer = setInterval(updateTimer, 66);
     } else {
       if (timer) {
-        clearInterval(timer)
+        clearInterval(timer);
       }
-    } 
+    }
 
     return () => {
       if (timer) {
         clearInterval(timer);
       }
     };
-  },[playing])
-  
-  
+  }, [playing]);
+
   useEffect(() => {
     return soundt
       ? () => {
@@ -98,14 +106,24 @@ const MusicPlayer = () => {
 
   return (
     <View style={styles.container}>
-        <MenuButton icon={"folder"} styles={[ styles.icon]} action={chooseFile} />
-        <Text>{songName}</Text>
+      <MenuButton icon={"folder"} styles={[styles.icon]} action={chooseFile} />
+      <Text>{songName}</Text>
       <View style={styles.menu}>
-
-        <MenuButton icon={"fast-forward"} styles={[styles.flip, styles.icon]} action={playSound} />
-        <MenuButton icon={playing ? "pause" : "play"} styles={[styles.icon]} action={playSound} />
-        <MenuButton icon={"fast-forward"} styles={[styles.icon]} action={playSound} />
-
+        <MenuButton
+          icon={"fast-forward"}
+          styles={[styles.flip, styles.icon]}
+          action={playSound}
+        />
+        <MenuButton
+          icon={playing ? "pause" : "play"}
+          styles={[styles.icon]}
+          action={playSound}
+        />
+        <MenuButton
+          icon={"fast-forward"}
+          styles={[styles.icon]}
+          action={playSound}
+        />
       </View>
       {duration !== undefined ? (
         <Bar duration={duration} position={position} />
@@ -119,7 +137,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    width: "100%"
+    width: "100%",
   },
   menu: {
     alignItems: "center",
